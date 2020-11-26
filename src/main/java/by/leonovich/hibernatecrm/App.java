e263d0bcd334e75dd4468007e3691e4b36ff6a47
CyberAlexander <festivdom@gmail.com>
2020-11-26 14:44:30 +0300

    Initial commit

---
+++ src/main/java/by/leonovich/hibernatecrm/App.java
@@ -0,0 +1,13 @@
+package by.leonovich.hibernatecrm;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
