2d66c6bbf3b2847c94957061565122f992434fa6
CyberAlexander <taurum13@gmail.com>
2021-10-01 10:47:15 +0300

    Tried var feature

--- hibernate-crm-main/src/main/java/by/leonovich/hibernatecrm/App.java
+++ hibernate-crm-main/src/main/java/by/leonovich/hibernatecrm/App.java
@@ -1,11 +1,11 @@
 package by.leonovich.hibernatecrm;
 
+import by.leonovich.hibernatecrm.exception.HibernateCrmApplicationException;
 import by.leonovich.hibernatecrm.mappings.singletable.Person;
 import by.leonovich.hibernatecrm.service.exception.ServiceException;
-import by.leonovich.hibernatecrm.service.person.PersonService;
+import by.leonovich.hibernatecrm.service.person.PersonServiceImpl;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import java.util.stream.Stream;
@@ -14,13 +14,13 @@ public class App {
     protected static final Logger LOGGER = LoggerFactory.getLogger(App.class);
 
     public static void main( String[] args ) throws Exception {
-        ApplicationContext context = new AnnotationConfigApplicationContext(ServiceConfiguration.class);
-        PersonService<Person> personService = context.getBean(PersonService.class);
+        var context = new AnnotationConfigApplicationContext(ServiceConfiguration.class);
+        var personService = context.getBean("personServiceImpl", PersonServiceImpl.class);
         Stream.generate(Person::init).limit(3).forEach(p -> {
             try {
                 personService.create(p);
             } catch (ServiceException e) {
-                throw new RuntimeException(e);
+                throw new HibernateCrmApplicationException(e);
             }
         });
 
